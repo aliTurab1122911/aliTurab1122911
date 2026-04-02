1ccb8b94441d4f120f54e3c5e7d5ea0ff7462a6c
aliTurab1122911 <aliturabulhussnain1122911@gmail.com>
2026-04-02 10:49:00 +0500

    d

--- middleware/session-data.js
+++
@@ -1,7 +0,0 @@
-function sessionData(req, res, next) {
-  res.locals.currentUser = req.session.user || null;
-  res.locals.cartCount = (req.session.cart || []).reduce((sum, item) => sum + item.quantity, 0);
-  next();
-}
-
-module.exports = sessionData;
